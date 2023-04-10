f8f782d417417f278ecf5d7f8fe26f09569f1c37
Vannesli <nezli2808@gmail.com>
2023-04-10 21:25:05 +0700

    js beres

--- script.js
+++ script.js
@@ -6,24 +6,6 @@ button.addEventListener('click', () => {
     const result = document.getElementById('output');
     let height_status=false, weight_status=false;
 
-    let gambar = [
-        {
-            status: "Kurus",
-            img: "https://images.unsplash.com/photo-1634463278803-f9f71890e67d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    
-        },
-        {
-            status: "Normal",
-            img: "https://images.unsplash.com/photo-1514409101893-7db2a465592d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    
-        },
-        {
-            status: "Gendut",
-            img: "https://images.unsplash.com/photo-1518859154638-ca29363763d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-    
-        },
-    ];
-
     if(height === '' || isNaN(height) || (height <= 0)){
         document.getElementById('height_error').innerHTML = 'Inputan Salah';
     }else{
@@ -54,5 +36,3 @@ button.addEventListener('click', () => {
         result.innerHTML = 'Silahkan Input Ulang';
     }
 });
-
-let listGambar = document.querySelector("#list-gambar")
